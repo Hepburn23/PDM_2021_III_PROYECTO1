65771f78f7b719ccee8a3a2c847dcd1350f24192
JRyako <johnny.elvir@ujcv.edu.hn>
2021-10-10 00:51:11 -0600

    FrontProducts

--- app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p1_proyecto1/MainActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p1_proyecto1/MainActivity.kt
@@ -1,11 +1,37 @@
 package hn.edu.ujcv.pdm_2021_iii_p1_proyecto1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Handlers
+        btnProducts.setOnClickListener {
+            productActivity()
+        }
+
+        btnClients.setOnClickListener {
+            clientActivity()
+        }
+
     }
+
+    //FUNCIONES
+
+    fun productActivity(){
+        val intent = Intent(this,ProductosActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun clientActivity(){
+        val intent = Intent(this,ClientesActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
--- app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p1_proyecto1/ProductosActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p1_proyecto1/ProductosActivity.kt
@@ -2,10 +2,24 @@ package hn.edu.ujcv.pdm_2021_iii_p1_proyecto1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_productos.*
 
 class ProductosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_productos)
+
+
+        //Handler
+        btnSave.setOnClickListener {
+            saveProd()
+        }
+
+    }
+
+
+
+    fun saveProd(){
+
     }
 }
