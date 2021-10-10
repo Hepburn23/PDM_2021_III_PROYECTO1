71b72d4adf002befba991106f3135226554ada61
Hepburn <91041472+Hepburn23@users.noreply.github.com>
2021-10-10 07:39:02 -0600

    Añadiendo clases restantes, mejorando la clase clientes, y trabajando en los layout de cada clase.

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p1_proyecto1/FacturasDeVentasActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_iii_p1_proyecto1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FacturasDeVentasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facturas_de_ventas)
+    }
+}
---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p1_proyecto1/FacturasPorIdentidadActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_iii_p1_proyecto1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FacturasPorIdentidadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facturas_por_identidad)
+    }
+}
